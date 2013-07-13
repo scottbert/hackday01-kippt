f27c107c77e6c2abc6dbac295d88f3cdc1dd0f55
Abid Din <hello@craftedpixelz.co.uk>
2013-07-13 13:34:29 +0100

    minor updates to app.js and initial commits of workstream

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
     var AppConfig = require('./bin/app_config'),
         Kippt = require('./bin/kippt_clips');
 
-        console.log(Kippt.KipptClips);
-        Kippt.KipptClips.init();
-        //var KipptClips = kippt('akqa', '3076c399fb4c7c337f5b3499ce6c3854658a4cc0');
+    var Clips = Kippt.KipptClips.init();
+    
 })();
