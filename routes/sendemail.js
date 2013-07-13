53f67de703ce851d4c39aae66ae1af3abd54f577
Scott van Looy <scott@quul.net>
2013-07-13 16:31:15 +0100

    Changed name of method

--- routes/sendemail.js
+++ routes/sendemail.js
@@ -6,35 +6,14 @@
 
 exports.index = function (req, res) {
     "use strict";
-    var nodemailer = require('nodemailer'),
-        smtpTransport = nodemailer.createTransport("mandrill", {
-            auth: {
-                user: "martin.shaw@akqa.com",
-                pass: "PRgF89ZeBH2oJyCBmpNkKQ"
-            }
-        }),
+    var mailer = require('./mailer'),
         fs = require('fs'),
-        str = fs.readFileSync('./views/email.ejs', 'utf8'),
-        mailOptions = {
-            from: "Fred Foo ✔ <sickpuppy@gmail.com>", // sender address
-            to: "akqa.hackathon@gmail.com", // list of receivers
-            subject: "AKQA hack day test email", // Subject line
-            html: str,
-            generateTextFromHTML: true
-        };
-    smtpTransport.sendMail(mailOptions, function (error, response) {
+        str = fs.readFileSync('./views/email.ejs', 'utf8');
+    mailer.sendEmail(str, function() {
         res.render('emailconfirm', {
             title: 'Send test email',
             layout: 'layout.ejs',
             pagetitle: 'Send a test email'
         });
-        if (error) {
-            console.log(error);
-        } else {
-            console.log("Message sent: " + response.message);
-        }
-
-        // if you don't want to use this transport object anymore, uncomment following line
-        smtpTransport.close(); // shut down the connection pool, no more messages
     });
 };
