26a4d6bdbb53559649701d71b51fed034e2de271
robinweston <lcfcforever@hotmail.com>
2013-07-13 14:08:31 +0100

    Moved scheduler config to main config object

--- bin/app_config.js
+++ bin/app_config.js
@@ -3,5 +3,10 @@ exports.AppConfig = {
         username: 'akqa',
         api_token: '3076c399fb4c7c337f5b3499ce6c3854658a4cc0',
         frequency: '7'
-    }
+    },
+	Scheduler: {
+		dayOfWeek: 6, // saturday
+		hour: 15,
+		minute: 00
+	}
 } 
