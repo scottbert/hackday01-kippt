9573c0cbe00517d59e1501325ef2f4010a9b83e1
Scott van Looy <scott@quul.net>
2013-07-13 14:10:08 +0100

    Fixes for app config

--- bin/kippt_clips.js
+++ bin/kippt_clips.js
@@ -1,5 +1,5 @@
-/*jslint bitwise: false, browser: true, es5: true, windows: false, evil: false, nomen: true, white: true, plusplus: true, vars: true, evil:true, regexp:true */
-/*global require:false, process:false, console:false, __dirname:false*/
+/*jshint bitwise:false, curly:true, eqeqeq:true, forin:true, immed:true, latedef:true, newcap:true, noarg:true, noempty:false, nonew:true, plusplus:false, regexp:false, undef:true, strict:true, trailing:true, expr:true, regexdash:true, browser:true, jquery:true, onevar:true */
+/*global require:false, process:false, console:false, __dirname:false, exports:false */
     var nodeKippt = require('node-kippt');
     var Config = require('../bin/app_config');
 
