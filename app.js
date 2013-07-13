8f6f53b858eba8aba572b6b24a40f21c185297ff
Scott van Looy <scott@quul.net>
2013-07-13 13:40:04 +0100

    test

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
         path = require('path'),
         http = require('http'),
         expressLayouts = require('express-ejs-layouts'),
-        //app = express(),
+        app = express(),
 
         // Global Application settings
         AppConfig = require('./bin/app_config'),
@@ -21,7 +21,6 @@
 
         // ROUTES OBJECTS
         sendemail = require('./routes/sendemail');
-        
     // A collection of Clips from Kippt
     //var Clips = Kippt.KipptClips.init();
 
