c2276cc12a4dbd1062c519dca05e2d9f1f7b2dbc
Abid Din <hello@craftedpixelz.co.uk>
2013-07-13 13:31:19 +0100

    added Kippt Api Workstream

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 (function() {
-    var kippt = require('node-kippt'),
-        mandrill = require('node-mandrill')('<Your Api Key Here>');
-        express = require('express'),
-        app = express();
-        kipptInstance = require('./bin/kippt_config');
-});
+    var AppConfig = require('./bin/app_config'),
+        Kippt = require('./bin/kippt_clips');
+
+        console.log(Kippt.KipptClips);
+        Kippt.KipptClips.init();
+        //var KipptClips = kippt('akqa', '3076c399fb4c7c337f5b3499ce6c3854658a4cc0');
+})();
