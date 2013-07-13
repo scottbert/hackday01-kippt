3e51c95e71fce0373f3be32c8d2d680b33a2e486
robinweston <lcfcforever@hotmail.com>
2013-07-13 15:14:42 +0100

    Tidied up app.js

--- app.js
+++ app.js
@@ -22,25 +22,33 @@
         // ROUTES OBJECTS
         sendemail = require('./routes/sendemail');
         
-    // A collection of Clips from Kippt
-    //var Clips = Kippt.KipptClips.init();
-
-    app.configure(function () {
-        app.set('views', __dirname + '/views');
-        app.set('port', 9998);
-        app.set('view engine', 'ejs');
-        app.use(expressLayouts);
-        app.use(express.static(path.join(__dirname, 'public')));
-        app.use(app.router);
-    });
-    app.configure('development', function () {
-        app.use(express.errorHandler());
-    });
-    // ROUTES
-    app.get('/sendemail', sendemail.index);
-    app.get('/', sendemail.index);
-    startup.start(process.argv, function(){console.log('I iz callback');});
-    http.createServer(app).listen(app.get('port'), function () {
-        console.log("Express server listening on port " + app.get('port'));
-    });
+		configureExpress(app);
+		
+		var schedulerFired = function(){
+			console.log("scheduler fired");
+		}
+		
+		startup.start(process.argv, schedulerFired);
+	
+	function configureExpress(app)
+	{
+		console.log("configuring express");
+	
+		app.configure(function () {
+			app.set('views', __dirname + '/views');
+			app.set('port', 9998);
+			app.set('view engine', 'ejs');
+			app.use(expressLayouts);
+			app.use(express.static(path.join(__dirname, 'public')));
+			app.use(app.router);
+		});
+		app.configure('development', function () {
+			app.use(express.errorHandler());
+		});
+		// ROUTES
+		app.get('/sendemail', sendemail.index);
+		app.get('/', sendemail.index);
+		
+		console.log("configured express");
+	}
 }());
